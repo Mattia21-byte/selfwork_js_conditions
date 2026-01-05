484351ac09f5b90dc4e30cb4068116bac0730e38
Mattia21-byte <mattiago2105@gmail.com>
2026-01-05 14:22:33 +0100

    selfwork cicli: tabellina con for e while

---
+++ selfwork_cicli_1.js
@@ -0,0 +1,19 @@
+// uso ciclo for
+let num = 2;
+
+for(let i = 1; i <= 10; i++){
+    console.log(num * i);
+    
+}
+
+// uso ciclo while
+
+let num2 = 2;
+let i = 1;
+while(i <=10){
+    console.log(num2 * i);
+
+    i++;
+    
+
+}
